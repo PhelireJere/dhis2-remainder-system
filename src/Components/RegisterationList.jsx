31c83a7615efaf2930c4fbf6ffb6713fb094c244
Phelire01 <phelirejere5@gmail.com>
2024-11-12 21:25:33 +0200

    registration list

--- src/Components/RegisterationList.jsx
+++ src/Components/RegisterationList.jsx
@@ -2,13 +2,13 @@ import React, {useState, useEffect} from 'react';
 import 'src/Components/RegisterationList.css';
 
 const RegisterationList= () => {
-  const [registeredPtients, setRegisteredPatients] = useState([]);
+  const [formData, setFormData] = useState({firstName:'', lastName:'' });
 
 
   useEffect(() => {
-    const storedData = localStorage.getItem('registeredPatients');
+    const storedData = localStorage.getItem('formData');
     if (storedData) {
-      setRegisteredPatients(JSON.parse(storedData));
+      setFormData(JSON.parse(storedData));
     }
   }, []);
 
@@ -16,17 +16,10 @@ const RegisterationList= () => {
     <div className = "container">
        <h2 className='heading'>Registered Patients </h2>
            
-           {registeredPatients.length > 0 ? 
-             (registeredPatients.map((patient, index) => (
-          
               <div key={index}className ="patient-card">
                  <p><strong>First Name: </strong>{patient.firstName}</p>
                  <p><strong>Last Name: </strong>{patient.lastName}</p>
               </div>
-             ))
-            ) : (
-              <p className='no-patients'>No registered patients</p>
-            )}
 
     </div>
   );
