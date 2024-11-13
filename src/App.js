597c64b3725141009e33be0c44cc3f998163415d
jahvillani <bsc-inf-13-21@unima.ac.mw>
2024-11-13 11:06:55 +0200

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Scheduling from './Components/Schedulling';
+
+function app(){
+    return (
+        <div className="app">
+            <Scheduling />
+        </div>
+
+    );
+}
+
+export default app;
