f408814af7159d5ca426180a935bcce8e2c83b39
Phelire01 <phelirejere5@gmail.com>
2024-11-20 09:18:47 +0200

    app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Register from "./Components/Register"
+
+function app() {
+    return (
+        <div className="app">
+           <Register/>
+        </div>
+    );
+}
+
+export default app;
