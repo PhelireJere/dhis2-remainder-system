04ffc71eed3b7c93b7fdccfe7c62a9075f1aeb53
pre-pukulu <bed-com-50-21@unima.ac.mw>
2024-11-30 22:05:34 +0200

    adding dashboad welcoming page in dashdoard

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Dashboard.css"
+
+const Dashboard = () => {
+  return(
+    <div className="dashboard">
+    <div className="welcome-message">
+      <h1>WELCOME TO GROUP 8 </h1>
+      <h2>DHIS2 PATIENT REMAINDER SYSTEM</h2>
+    </div>
+  </div>
+    
+  );
+};
+
+export default Dashboard;
+
+
+{/* <div className="dashboard-container">
+
+<h1>WELCOME TO GROUP 8 <br/>DHIS2 PATIENT REMAINDER SYSTEM</h1>
+
+</div> */}
